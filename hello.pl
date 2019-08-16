3bda0d912130f5cf1e61334c3aa163a941032ab6
tucker76 <tucker76@gmail.com>
2019-08-16 15:21:42 +0000

    my first push

--- hello.pl
+++ hello.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl
 
 #Author - Vijay Nagarajan PhD
-#print a string - hello vijay
-print "Hello Raj\n";
+#print a string - hello matt
+print "Hello matt\n";
 
-
+git config --global user.name "matt"
+git config --global user.email "tucker76@gmail.com"
+git config --global credential.helper "cache --timeout=3600"
+git config --global push.default matching
 
 
